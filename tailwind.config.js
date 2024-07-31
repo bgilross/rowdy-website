70e12e596368beecbde76685ebc152dd7d15a607
bgilross <bgilross@gmail.com>
2024-07-30 19:03:47 -0700

    fixecd background image, fucked around with icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        carnivale: ["CarnivaleeFreakshow", "serif"],
-        bronco: ["BroncoPersonalUse", "serif"],
+        carnivale: ['CarnivaleeFreakshow', 'serif'],
+        bronco: ['BroncoPersonalUse', 'serif'],
+      },
+      backgroundImage: {
+        background: "url('images/rowdy-pic-1.png')",
       },
     },
   },
   plugins: [],
-};
+}
