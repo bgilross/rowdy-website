d3cce3ec3f59ca24364f8f0db66688306c2e6dbc
Jim <hippiejim2896@gmail.com>
2024-08-01 01:34:50 -0500

    Add more responsive styling in 'Hero' component to alleviate drift of social icons

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -20,12 +20,28 @@ const Hero = () => {
         <h1 className='text-3xl px-6'>SUPER GROUP</h1>
       </div>
       {/* <Background /> */}
-      <div className='pt-8 flex mb-8 px-32 bg-black md:bg-background bg-contain bg-center bg-no-repeat h-full w-full justify-around items-center'>
-        <div className='flex md:items-center justify-around items-end h-[70%] w-full md:w-[50%] lg:w-[40%] xl:w-[30%]'>
-          <img src={facebook} alt='facebook' className='w-20 h-20' />
-          <img src={instagram} alt='facebook' className='w-24 h-24' />
-          <img src={youtube} alt='facebook' className='w-20 h-20' />
-          <img src={spotify} alt='facebook' className='w-24 h-24' />
+      <div className='pt-8 flex mb-8 px-32 bg-black md:bg-background bg-contain bg-center bg-no-repeat h-full w-full justify-center items-center'>
+        <div className='flex md:items-center justify-center items-end h-[70%] w-[60%] md:w-[50%] lg:w-[40%] xl:w-[30%]'>
+          <img
+            src={facebook}
+            alt='facebook'
+            className='w-20 h-20 mx-2 md:mx-4'
+          />
+          <img
+            src={instagram}
+            alt='facebook'
+            className='w-24 h-24 mx-2 md:mx-4'
+          />
+          <img
+            src={youtube}
+            alt='facebook'
+            className='w-20 h-20 mx-2 md:mx-4'
+          />
+          <img
+            src={spotify}
+            alt='facebook'
+            className='w-24 h-24 mx-2 md:mx-4'
+          />
         </div>
       </div>
     </div>
