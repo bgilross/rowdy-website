bdf4bea3e5e2d3f3cd8fba65990c9f1a25180734
Jim <hippiejim2896@gmail.com>
2024-08-02 17:36:43 -0500

    Add content to 'Bio' component

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -3,7 +3,108 @@ import React from "react";
 const Bio = () => {
   return (
     <div className='flex flex-col w-full h-screen text-white'>
-      <div>BIO</div>
+      <div className='flex w-full h-[25%] px-8'>
+        <ul className='flex items-center justify-between'>
+          <li>
+            <img
+              className='px-2'
+              src='https://plus.unsplash.com/premium_photo-1663046050988-1b873a56dced?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              alt='Img 1'
+            ></img>
+          </li>
+          <li>
+            <img
+              className='px-2'
+              src='https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?q=80&w=1816&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              alt='Img 2'
+            ></img>
+          </li>
+          <li>
+            <img
+              className='px-2'
+              src='https://images.unsplash.com/photo-1624636224909-d986525fb391?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              alt='Img 3'
+            ></img>
+          </li>
+          <li>
+            <img
+              className='px-2'
+              src='https://images.unsplash.com/photo-1563900319-f97c9fcc5843?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              alt='Img 4'
+            ></img>
+          </li>
+          <li>
+            <img
+              className='px-2'
+              src='https://images.unsplash.com/photo-1629276300935-8177a50f2626?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              alt='Img 5'
+            ></img>
+          </li>
+        </ul>
+      </div>
+      <div className='flex flex-col h-[50%] w-full items-center justify-center'>
+        <div className='flex text-white/80 text-[3rem] sm:text-[5rem] md:text-[7rem] font-carnivale pb-8'>
+          <h1 className='rowdy'>R</h1>
+          <h1 className='rowdy'>O</h1>
+          <h1 className='rowdy'>W</h1>
+          <h1 className='rowdy'>D</h1>
+          <h1 className='rowdy'>Y</h1>
+        </div>
+        <h1 className='font-bronco text-sm md:text-lg lg:text-xl xl:text-3xl px-24 pb-8'>
+          From the depths of the bayou city comes a multigenerational gathering
+          of some of the scenes greatest individuals that came together to
+          create what some say is a: PASADENA, TEXAS SUPER GROUP years in the
+          making. Nothing falls short on these five fellas shoulders. They have
+          been noted as saying, "We're not trying to reinvent the wheel here,
+          we're just putting 4x4 tires on it". With major influences of southern
+          rock, groove metal, rockin riffs, heavy metal and a whole host of
+          other extravagancies. You will leave their live set begging for more.
+          A complete working man's band, they are constantly putting out new
+          material and videos to boot. Conquering Texas is the short term goal.
+          The big picture has them playing on major stages across the greatest
+          planet in our solar system. With the moniker "LET'S GET ROWDY!" how
+          can you not be infatuated with this super-group five piece?
+        </h1>
+      </div>
+      <div className='flex w-full h-[25%] px-8'>
+        <ul className='flex items-center justify-between'>
+          <li>
+            <img
+              className='px-2'
+              src='https://plus.unsplash.com/premium_photo-1663046050988-1b873a56dced?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              alt='Img 1'
+            ></img>
+          </li>
+          <li>
+            <img
+              className='px-2'
+              src='https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?q=80&w=1816&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              alt='Img 2'
+            ></img>
+          </li>
+          <li>
+            <img
+              className='px-2'
+              src='https://images.unsplash.com/photo-1624636224909-d986525fb391?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              alt='Img 3'
+            ></img>
+          </li>
+          <li>
+            <img
+              className='px-2'
+              src='https://images.unsplash.com/photo-1563900319-f97c9fcc5843?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              alt='Img 4'
+            ></img>
+          </li>
+          <li>
+            <img
+              className='px-2'
+              src='https://images.unsplash.com/photo-1629276300935-8177a50f2626?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              alt='Img 5'
+            ></img>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
