ade25a00cd6d2c8a7c579ab72c2a969f34928634
Jim <hippiejim2896@gmail.com>
2024-08-02 17:50:26 -0500

    Set secondary background image details in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
       },
       backgroundImage: {
         background: "url('images/rowdy-pic-cropped1.png')",
+        secondary:
+          "url('https://images.unsplash.com/photo-1655183806422-e40565e79e5c?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
       },
     },
   },
