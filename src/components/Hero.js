dc5d92f2cd5f00b7f20f290216544894c9c6b48c
bgilross <bgilross@gmail.com>
2024-08-05 22:59:36 -0700

    add social component, social icon responsiveness, minor styling hamburger menu,

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,14 +5,15 @@ import React from 'react'
 // import youtube from '../images/youtube2.png'
 // import spotify from '../images/spotify2.png'
 
-import facebook2 from '../images/facebook.svg'
-import instagram2 from '../images/instagram.svg'
-import spotify2 from '../images/spotify.svg'
-import youtube2 from '../images/youtube.svg'
+// import facebook2 from '../images/facebook.svg'
+// import instagram2 from '../images/instagram.svg'
+// import spotify2 from '../images/spotify.svg'
+// import youtube2 from '../images/youtube.svg'
+import Social from './Social'
 
 const Hero = () => {
   return (
-    <div className="flex flex-col bg-background bg-contain bg-center bg-no-repeat md:bg-none h-screen w-full items-center justify-center md:justify-between bg-black ">
+    <div className="flex flex-col bg-contain bg-center bg-no-repeat h-screen w-full items-center justify-center md:justify-between bg-black ">
       <div className="flex text-white md:text-white/80 text-[4rem] sm:text-[6rem] md:text-[8rem] font-carnivale">
         <h1 className="rowdy">R</h1>
         <h1 className="rowdy">O</h1>
@@ -25,13 +26,8 @@ const Hero = () => {
         <h1 className="px-6">SUPER GROUP</h1>
       </div>
       {/* <Background /> */}
-      <div className="pt-8 hidden md:flex mb-8 px-32 bg-black md:bg-background bg-contain bg-center bg-no-repeat h-full w-full justify-center items-center">
-        <div className="flex md:items-center justify-center md:w-[50%] lg:w-[40%] xl:w-[30%]">
-          <img src={instagram2} alt="facebook" className="icons" />
-          <img src={youtube2} alt="facebook" className="icons" />
-          <img src={spotify2} alt="facebook" className="icons" />
-          <img src={facebook2} alt="facebook" className="icons" />
-        </div>
+      <div className="pt-8 md:flex mb-8 px-32 bg-black md:bg-background bg-contain bg-center bg-no-repeat h-full w-full justify-center items-center">
+        <Social />
       </div>
     </div>
   )
